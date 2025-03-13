cd2795c400bb924a64843d527f81df64dbf0af69
AnahatM <anahat.mudgal@gmail.com>
2025-03-12 22:31:58 -0400

    Page Navigation

---
+++ lib/components/my_drawer.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:minimalist_music_player/pages/settings_page.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        children: [
+          // Logo
+          DrawerHeader(
+            child: Center(
+              child: Icon(
+                Icons.music_note,
+                size: 40,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+          ),
+
+          // Home Tile
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, top: 25.0),
+            child: ListTile(
+              title: Text("HOME"),
+              leading: Icon(Icons.home),
+              onTap: () {},
+            ),
+          ),
+
+          // Settings Tile
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, top: 25.0),
+            child: ListTile(
+              title: Text("SETTINGS"),
+              leading: Icon(Icons.settings),
+              onTap: () {
+                // Pop drawer
+                Navigator.pop(context);
+                // Navigate to settings page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SettingsPage()),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minimalist_music_player/components/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Theme.of(context).colorScheme.surface);
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(title: Text("PLAYLIST"), centerTitle: true),
+      drawer: const MyDrawer(),
+    );
   }
 }
